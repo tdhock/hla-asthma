a3dc13ac5a02302b75eb59538b6c8f63d2fb07a1
Toby Dylan Hocking <tdhock5@gmail.com>
2017-09-15 12:32:18 -0400

    old scales

--- figure-asthma.R
+++ figure-asthma.R
@@ -6,7 +6,11 @@ load("input.compare.RData")
 load("input.features.RData")
 feature.sets$markers %in% input.compare$selected$variable
 
-variable.counts <- input.compare$selected[, list(
+some <- function(dt){
+  dt[input.name != "hla.oldMarkers.sex"]
+}
+
+variable.counts <- some(input.compare$selected)[, list(
   mean.weight=mean(weight),
   folds=.N), by=.(variable, output.name, input.name)]
 setkey(variable.counts, variable, output.name, input.name)
@@ -14,9 +18,29 @@ sorted.counts <- variable.counts[order(-folds, -abs(mean.weight)),]
 variable.counts[, variable.fac := factor(variable, rev(unique(paste(sorted.counts$variable))))]
 stopifnot(all(!is.na(variable.counts$variable.fac)))
 setkey(input.compare$selected, variable, output.name, input.name)
-all.points <- input.compare$selected[variable.counts]
+all.points <- some(input.compare$selected[variable.counts])
 
-test.roc <- data.table(input.compare$roc)
+some.counts <- variable.counts[input.name=="hla.markers.sex"]
+levs <- some.counts[order(folds, mean.weight), variable]
+some.points <- all.points[input.name=="hla.markers.sex" & folds==4]
+some.points[, variable.fac := factor(variable, levs)]
+some.points[, variable.type := ifelse(variable=="sex1", "sex", ifelse(grepl("rs", variable), "marker", "hla"))]
+some.points[, table(variable.type, test.fold)]
+gg <- ggplot()+
+  ggtitle("Features used in models to predict asthma in all 4 cross-validation folds\n12/328 hla, 31/32 markers, 44/361 total")+
+  geom_vline(xintercept=0, color="grey")+
+  scale_color_brewer(palette="Dark2")+
+  theme_bw()+
+  geom_point(aes(weight, variable.fac, color=variable.type),
+             data=some.points, shape=1)+
+  xlab("input feature weight / effect size")+
+  ylab("input feature")
+pdf("../jobs-dec-2016/figure-asthma-4folds.pdf")
+print(gg)
+dev.off()
+some.points[variable.type=="marker",  table(folds)]
+
+test.roc <- some(input.compare$roc)[, {
   FPR.grid <- seq(0, 1, l=101)
   TPR.grid <- approx(FPR, TPR, FPR.grid)$y
   data.table(
@@ -31,24 +55,28 @@ ggplot()+
   coord_equal()+
   geom_path(aes(FPR, TPR, color=input.name), data=test.roc)
 
-test.dots <- data.table(input.compare$error)
+
+test.dots <- some(input.compare$error)
 test.means <- test.dots[, list(mean.auc=mean(auc)), by=input.name][order(mean.auc),]
-feature.counts <- sapply(feature.sets, length)
+feature.counts <- sort(sapply(feature.sets, length))
 dname <- function(input.name){
-  n.features <- feature.counts[input.name]
+  n.features <- feature.counts[paste(input.name)]
   sprintf("%s\n%d features", input.name, n.features)
 }
-levs <- dname(test.means$input.name)
+levs <- dname(names(feature.counts))
 test.dots[, input.fac := factor(dname(input.name), levs)]
 gg.old <- ggplot()+
-  xlab("Test AUC (4-fold cross-validation)")+
-  ylab("Input feature set")+
+  scale_x_continuous(
+    "Test AUC (4-fold cross-validation)",
+    limits=c(0.5, 0.61)
+    )+
+  scale_y_discrete("Input feature set")+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
   geom_point(aes(auc, input.fac),
              shape=1,
-data=test.dots)
-pdf("figure-asthma-old.pdf", h=3)
+             data=test.dots)
+pdf("figure-asthma-old.pdf", 4, 2.5)
 print(gg.old)
 dev.off()
 
@@ -61,10 +89,10 @@ gg <- ggplot()+
   theme_bw()+
   theme(panel.margin=grid::unit(0, "lines"))+
   facet_grid(. ~ output.name, labeller=function(df){
-##df$output.name <- gsub("[/]", "\n", df$output.name)
-df$output.name <- paste(df$output.name)
-df
-})+
+    ##df$output.name <- gsub("[/]", "\n", df$output.name)
+    df$output.name <- paste(df$output.name)
+    df
+  })+
   scale_linetype_manual(
     "input features",
     values=orig.types)+
@@ -77,10 +105,10 @@ df
     "True Positive Rate\n= Probability(Disease | Disease)",
     breaks=c(0, 0.5, 1))+
   geom_path(aes(FPR, TPR, group=paste(test.fold, input.name),
-linetype=input.name,
-color=input.name),
-size=0.3,
-data=test.roc)+
+                linetype=input.name,
+                color=input.name),
+            size=0.3,
+            data=test.roc)+
   coord_equal()+
   geom_abline(slope=1, intercept=0, color="grey", size=0.3)+
   theme(legend.position="bottom")+
--- packages.R
+++ packages.R
@@ -1,4 +1,5 @@
 works_with_R("3.2.3",
+             "hadley/scales@7fc502c696799b6e9d2e4ef1b0aeffdd80cec0f8",
              "tdhock/ggplot2@a8b06ddb680acdcdbd927773b1011c562134e4d2",
              "tdhock/animint@2a462853491b985ee90e86ef7e017ecd51d2921f",
              "tdhock/directlabels@7b4b08a5dd0ab86e0b90902b3a233903ddd42311",
